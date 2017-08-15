49b78dd468f37443e98a0550d0efcef7f94a70ce
陈小七 <283775789@qq.com>
2017-08-15 22:29:08 +0800

    提交8月11号修改

--- projects/9包/src/static/js/normal.js
+++ projects/9包/src/static/js/normal.js
@@ -73,12 +73,72 @@ var webui = {
         var right = ($(window).width() - $('.g-body').width()) / 2;
         $affix.css('right', right);
     },
+    //初始化升缩文本的展开与收起
+    initRsText: function ($rsTexts) {
+        $rsTexts.each(function () {
+            var rstext = '',
+                dataparam = '',
+                charnum = 200,
+                subStr = '',
+                textLength = 0,
+                $riseSwitch = null,
+                $shrinkSwitch = null,
+                $temp = $('<div></div>'),
+                riseSwitchHtml = '<span class="u-rstext-switch" data-as="rstext-riseswitch">展开<span>︾</span></span>',
+                shrinkSwitchHtml = '<span class="u-rstext-switch" data-as="rstext-shrinkswitch">收起<span>︽</span></span>';
+
+            $riseSwitch = $(this).find('[data-as="rstext-riseswitch"]');
+            $shrinkSwitch = $(this).find('[data-as="rstext-shrinkswitch"]');
+
+            //如果存在自定义的展开开关，获取其对应的html
+            if ($riseSwitch.length > 0) {
+                $temp.append($riseSwitch);
+                riseSwitchHtml = $temp.html();
+            }
+
+            //如果存在自定义的关闭开关，获取其对应的html
+            if ($shrinkSwitch.length > 0) {
+                $temp = $('<div></div>');
+                $temp.append($shrinkSwitch);
+                shrinkSwitchHtml = $temp.html();
+            }
+
+            rstext = $(this).html();
+            textLength = rstext.length;
+            dataparam = $(this).data('param');
+
+            //如果指定了限制字符数且指定的字符数能转换为整数就用指定的字符数作限制字符数，否则默认最多显示200个字符
+            if (typeof dataparam != "undefined") {
+                charnum = isNaN(parseInt(dataparam)) ? 200 : parseInt(dataparam);
+            }
+
+            //仅当文本字符数大于限制字符个数时，才显示切换开关
+            if (textLength > charnum) {
+                subStr = rstext.substring(0, charnum);
+                $(this).html(subStr + '...' + riseSwitchHtml);
+            }
+
+            //单击展开开关:显示全部文字
+            $(this).on('click', '[data-as="rstext-riseswitch"]', function () {
+                var $rstext = $(this).parent();
+                $rstext.html(rstext + shrinkSwitchHtml);
+            });
+
+            //单击收起开关:截断部份文字
+            $(this).on('click', '[data-as="rstext-shrinkswitch"]', function () {
+                var $rstext = $(this).parent();
+                subStr = rstext.substring(0, charnum);
+                $rstext.html(subStr + '...' + riseSwitchHtml);
+            });
+        });
+    },
     //函数:初始化所有控件
     init: function () {
         var $datePicker = $('.js-datepicker'),
             $monthPicker = $('.js-monthpicker'),
             $yearPicker = $('.js-yearpicker'),
-            $affix = $('.m-affix');
+            $affix = $('.m-affix'),
+            $rsTexts = $('[data-as="rstext"]');
 
         if ($datePicker.length > 0) {
             webui.initDatePicker($datePicker);
@@ -98,6 +158,10 @@ var webui = {
             $(window).off('lazyResize.reui').on('lazyResize.reui', function () {
                 webui.initAffix($affix);
             });
+        }
+
+        if ($rsTexts.length > 0) {
+            webui.initRsText($rsTexts);
         }
     }
 }
